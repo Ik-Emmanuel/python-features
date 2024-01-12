6f6368e2906429fb235be6df3b833f7ad0c40e9d
Ik-Emmanuel <nwokochaikechukwu13@gmail.com>
2024-01-12 11:56:32 +0000

    new partials example

--- functions_functools/partial_example.py
+++ functions_functools/partial_example.py
@@ -1,4 +1,7 @@
+import logging
 from functools import partial
+from typing import Callable
+
 
 """
 partials preload functions and readies them to be called later
@@ -12,3 +15,40 @@ for i in my_list:
     new_func = partial(add_numbers, second_num=i)
     value = new_func(6)
     print(value)
+    
+    
+    
+
+##### Food ordering function 
+
+def handle_payment_stripe(amount: int) -> None:
+    logging.info(f"Charging ${amount/100:.2f} using Stripe")
+
+
+PRICES = {
+    "burger": 10_00,
+    "fries": 5_00,
+    "drink": 2_00,
+    "salad": 15_00,
+}
+
+
+HandlePaymentFn = Callable[[int], None]
+
+def order_food(items: list[str], payment_handler: HandlePaymentFn) -> None:
+    total = sum(PRICES[item] for item in items)
+    logging.info(f"Order total is ${total/100:.2f}.")
+    payment_handler(total)
+    logging.info("Order completed.")
+
+
+order_food_stripe = partial(order_food, payment_handler=handle_payment_stripe)
+
+def main() -> None:
+    logging.basicConfig(level=logging.INFO)
+    # order_food(["burger", "salad", "drink"], handle_payment_stripe)
+    order_food_stripe(["burger", "salad", "drink"])
+
+
+if __name__ == "__main__":
+    main()
