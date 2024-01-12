1efa53f686ba20398f0d71811d274b156384a010
Ik-Emmanuel <nwokochaikechukwu13@gmail.com>
2024-01-12 11:52:02 +0000

    protocol

--- protocol_classes/example.py
+++ protocol_classes/example.py
@@ -6,3 +6,32 @@ class ShapeProtocol(Protocol):
 
     def perimeter(self) -> float:
         ...
+        
+from dataclasses import dataclass
+
+@dataclass
+class Card:
+    number: str
+    exp_month: int
+    exp_year: int
+    valid: bool = False
+
+
+class CardInfo(Protocol):
+    @property
+    def number(self) -> str:
+        ...
+
+    @property
+    def exp_month(self) -> int:
+        ...
+
+    @property
+    def exp_year(self) -> int:
+        ...
+
+
+def validate_card(card: CardInfo) -> bool:
+    return (
+        True
+    )
